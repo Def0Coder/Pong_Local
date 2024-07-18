bd7ef31f3f2bc1594ceff4c6f50ca72a6e81dc39
Def0Coder <fm.defranceschi@gmail.com>
2024-07-18 22:53:40 +0200

    Basic Game

--- Pong_Local/Assets/Scripts/Ball_Movement.cs
+++ Pong_Local/Assets/Scripts/Ball_Movement.cs
@@ -1,3 +1,4 @@
+
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,55 +6,62 @@ using UnityEngine;
 
 public class Ball_Movement : NetworkBehaviour
 {
-    [SerializeField]
-    public float speed;
+    public float speed = 30;
     public Rigidbody2D rigidbody2d;
+    private Manager manager;
 
     public override void OnStartServer()
     {
         base.OnStartServer();
-
-
         rigidbody2d.simulated = true;
+        Vector2 randomDirection = Random.Range(0, 2) == 0 ? Vector2.right : Vector2.left;
+        rigidbody2d.velocity = randomDirection * speed;
+    }
 
+    void Start()
+    {
+        manager = FindObjectOfType<Manager>();
 
-        rigidbody2d.velocity = Vector2.right * speed;
+        if (manager == null)
+        {
+            Debug.LogError("Manager non trovato!");
+        }
     }
 
     float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
     {
- 
         return (ballPos.y - racketPos.y) / racketHeight;
     }
-
 
     [ServerCallback]
     void OnCollisionEnter2D(Collision2D col)
     {
-
-
         if (col.transform.GetComponent<Player_Moves>())
         {
-
-            float y = HitFactor(transform.position,
-                                col.transform.position,
-                                col.collider.bounds.size.y);
-
-
+            float y = HitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
             float x = col.relativeVelocity.x > 0 ? 1 : -1;
-
-
             Vector2 dir = new Vector2(x, y).normalized;
-
             rigidbody2d.velocity = dir * speed;
         }
 
-        else if(col.transform.CompareTag("WL") || col.transform.CompareTag("WR"))
+        if (col.gameObject.tag == "WL")
         {
-            Debug.Log("unto");
-
-            rigidbody2d.velocity = Vector2.zero;
+            manager.UpdateScore(2); // Player 2 scores
+            NetworkServer.Destroy(gameObject);
         }
+        else if (col.gameObject.tag == "WR")
+        {
+            manager.UpdateScore(1); // Player 1 scores
+            NetworkServer.Destroy(gameObject);
+        }
+        //else if (col.gameObject.tag == "Player1Goal" "Player2Goal")
+        //{
+        //    if (manager != null)
+        //    {
+        //        manager.UpdateScore(col.transform.position.x > 0 ? 1 : 2);
+        //    }
+        //    NetworkServer.Destroy(gameObject);
+        //}
     }
 }
 
--- Pong_Local_clone_0/Assets/Scripts/Manager.cs
+++ Pong_Local_clone_0/Assets/Scripts/Manager.cs
@@ -2,22 +2,78 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Manager : NetworkBehaviour
 {
-    public GameObject ballPrefab; // Assegna il prefab della palla dall'Inspector
+    public GameObject ballPrefab;
+
+    [SyncVar(hook = nameof(OnScoreP1Changed))]
+    public int scoreP1 = 0;
+
+    [SyncVar(hook = nameof(OnScoreP2Changed))]
+    public int scoreP2 = 0;
+
+    public TextMeshProUGUI Score1Text;
+    public TextMeshProUGUI Score2Text;
+
+    private GameObject currentBall;
 
     public override void OnStartServer()
     {
         base.OnStartServer();
+    }
+
+    void OnScoreP1Changed(int oldScore, int newScore)
+    {
+        Score1Text.text = "P1: " + newScore;
+        Debug.Log($"Punteggio Player 1 aggiornato: {newScore}");
+    }
+
+    void OnScoreP2Changed(int oldScore, int newScore)
+    {
+        Score2Text.text = "P2: " + newScore;
+        Debug.Log($"Punteggio Player 2 aggiornato: {newScore}");
+    }
+
+    [Server]
+    public void UpdateScore(int player)
+    {
+        if (player == 1)
+        {
+            scoreP1++;
+        }
+        else if (player == 2)
+        {
+            scoreP2++;
+        }
+
+        // Distrugge la palla corrente
+        if (currentBall != null)
+        {
+            NetworkServer.Destroy(currentBall);
+        }
+
+        // Spawna una nuova palla
         SpawnBall();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            SpawnBall();
+        }
     }
 
     [Server]
     void SpawnBall()
     {
-        // Crea la palla solo sul server
-        GameObject ball = Instantiate(ballPrefab, Vector3.zero, Quaternion.identity);
-        NetworkServer.Spawn(ball);
+        if (currentBall == null)
+        {
+            currentBall = Instantiate(ballPrefab, Vector3.zero, Quaternion.identity);
+            NetworkServer.Spawn(currentBall);
+        }
     }
 }
